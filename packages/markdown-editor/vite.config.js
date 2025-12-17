7e0c6d7df983591f92dde81b529222ccf522d656
zsj <zousjun@126.com>
2025-12-17 17:50:58 +0800

    develop zhihu sync

--- packages/markdown-editor/vite.config.js
+++ packages/markdown-editor/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      include: ['path', 'events', 'stream', 'util', 'buffer', 'process'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
   root: 'src',
   base: './',
   build: {
